93105a882cc21704f6daec68d2f8d7f65d80fe82
Haneen <haneensharar00@gmail.com>
2023-06-20 13:28:26 +0300

    adding task table and model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "pages#home"
+  root 'pages#home'
   # Defines the root path route ("/")
   # root "articles#index"
   get 'about' => 'pages#about'
