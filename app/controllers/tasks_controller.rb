c6d6ce8d58ba159b55b65331134a254fc4d85340
Haneen <haneensharar00@gmail.com>
2023-06-21 10:42:41 +0300

    edit and show links

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,43 +1,45 @@
-class TasksController < ApplicationController 
+class TasksController < ApplicationController
+  def index
+    @tasks= Task.all
+  end
 
-  def new 
+  def new
     @task = Task.new
   end
-  
+
   def edit
     @task = Task.find(params[:id])
   end
 
-  def create 
+  def create
     @task = Task.new(tasks_params)
     if @task.save
-        flash[:notice] = "Task was successfully created"
-        redirect_to task_path(@task)
-        
+      flash[:notice] = 'Task was successfully created'
+      redirect_to task_path(@task)
+
     else
-        render 'new'
+      render 'new'
     end
   end
 
-  def update 
+  def update
     @task = Task.find(params[:id])
     if @task.update(tasks_params)
-        flash[:notice] = "task was successfully updated"
-        redirect_to task_path(@task)
-    else 
-        render 'edit'
-    
-    end        
+      flash[:notice] = 'task was successfully updated'
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+
+    end
   end
 
-   def show
-     @task  = Task.find(params[:id])
-   end
+  def show
+    @task = Task.find(params[:id])
+  end
 
   private
 
   def tasks_params
     params.require(:task).permit(:content)
   end
-
-end 
+end
